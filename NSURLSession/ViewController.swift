3a666fef666602b9f57e30012d4a0f188423c0b7
mohamedosama2074 <iosengks74@gmail.com>
2019-09-09 09:56:10 +0200

    URLSessionDataTask

--- NSURLSession/ViewController.swift
+++ NSURLSession/ViewController.swift
@@ -10,11 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let session = URLSession.shared
+    
+    let configure = URLSessionConfiguration.default
+    
+    let configure2 = URLSessionConfiguration.ephemeral
+    
+    let configure3 = URLSessionConfiguration.background(withIdentifier: "Download")
+    
+    
+    // create session
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
+//        configure3.allowsCellularAccess = true
+        
+      
+
     }
 
+    
+    @IBAction func btn_task(_ sender: UIButton) {
+        
+        createTask()
+    
+    }
+    func createTask(){
+        
+        guard let url = URL(string:"https://www.facebook.com/profile.php?id=100013381152500") else {return}
+        
+        session.dataTask(with: url) { (data, response, error) in
+            
+            if let error = error{
+                
+                print(error)
+            }
+            
+            if let response = response {
+                
+                print(response)
+            }
+            
+            if let data = data {
+                
+                print(data)
+            }
+            
+        }.resume()
+        
+    }
 
 }
 
